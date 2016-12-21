2536f5600325257d3b25723a10bbd9aa795ee7b5
April Hudspeth <april.hudspeth@vantiv.com>
2016-12-21 10:45:23 -0700

    added a new test

--- src/spec/table-spec.js
+++ src/spec/table-spec.js
@@ -1,47 +1,54 @@
 import tableChart from 'table';
 import * as d3 from "d3";
 
-   var draw = tableChart();	
+  
+
+describe("Test the table chart", function(){
+
+	var draw = tableChart();	
 
 
 	var data = [{"c1": 1, "c2": 2, "c3": 3},{"c1": 5, "c2": 6, "c3": 7}];
 	data.columns = ["c1", "c2", "c3"];
 
-	
-
-describe("Test the table chart", function(){
-
 	beforeEach(function(){
 
-		var el = d3.select("html").append("body");
-
-		var table = d3.select("body")
-	    		.append("table")
-	    		.attr("class", "table");
+		// build out the HTML elements on the DOM first before calling the tableChart
+		// function
+		var el = d3.select("body").append("div").attr("id", "drawtable");
+		var table = d3.select("#drawtable")
+    		.append("table")
+    		.attr("class", "table");
+		
+		table.append("thead");
+ 		table.append("tbody");
 		
 		draw(table, data);
 
 	});
 
 	afterEach(function(){
+
+		//clear out the table element after each test
 		d3.selectAll('table').remove();
 	});
 
 
-	describe('the table', function(){
-		it('should be created', function() {
+	it('should be created, not null and defined', function() {
 
-
-			expect(d3.selectAll('tr')._groups[0][0]).not.toBeNull();
-			expect(d3.selectAll('tr')._groups[0][0]).toBeDefined();
+		expect(d3.selectAll('tr')._groups[0][0]).not.toBeNull();
+		expect(d3.selectAll('tr')._groups[0][0]).toBeDefined();
 
 			
-		});
-
-		it('should build a table with one row', function(){
-			console.log(d3.selectAll('tbody').selectAll('tr')._groups[0]);
-			expect(d3.select('tbody').selectAll('tr')._groups[0].length).toEqual(2);
-		});
 	});
+
+	it('should build a table with two rows', function(){
+			
+		expect(d3.select('tbody').selectAll('tr')._groups[0].length).toEqual(2);
+	});
+	
+	it('the tabl in #drawtable div should have the class of table', function() {
+		expect(d3.select("#drawtable").select("table")._groups[0][0].className).toEqual("table");
+	})
 
 });
